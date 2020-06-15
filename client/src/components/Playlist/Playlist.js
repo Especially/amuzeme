b11566cc032e18986b1e02c988dcd8050d2dcb43
Especially <_j@live.ca>
2020-06-15 10:50:02 -0400

    Camera upload fully functionl, working on max 3 images

---
+++ client/src/components/Playlist/Playlist.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import './Playlist.scss';
+
+function Playlist({images}) {
+    // const [selfies, setSelfies] = useState([]);
+
+    // useEffect(() => {
+    //     if (images.length > 0) {
+    //         setSelfies(images);
+    //     }
+    // }, [selfies])
+
+
+    return (
+        <>
+            <section className="playlist">
+
+            </section>
+        </>
+    );
+}
+
+export default Playlist;
